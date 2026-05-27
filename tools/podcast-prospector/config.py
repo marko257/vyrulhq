b550a8ad98df101fb53c1f9be2c9b8cc9786777f
marko257 <marko@riseadvertising.co>
2026-05-26 17:36:42 -0700

    feat: scaffold podcast-prospector tool

---
+++ tools/podcast-prospector/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LISTEN_NOTES_API_KEY = os.environ['LISTEN_NOTES_API_KEY']
+YOUTUBE_API_KEY = os.environ['YOUTUBE_API_KEY']
+
+LISTEN_NOTES_BASE_URL = 'https://listen-api.listennotes.com/api/v2'
+YOUTUBE_SHORTS_MAX_SECONDS = 60
+LOOKBACK_DAYS = 90
+MIN_EPISODES = 50
